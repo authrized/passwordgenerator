6e41d449fc0901fddf755b73164aa1da30a98a59
Elijah Friedman <elijah.friedman@authrized.com>
2024-05-11 07:12:22 -0400

    Update generator.swift

--- generator.swift
+++ generator.swift
@@ -1,8 +1,7 @@
-    func generatePassword(length: Int, withSymbols: Bool = true) -> String { // The function that can be called
-        var characters = letters + numbers // Makes the characters to use
-        if withSymbols {
+    func generatePassword(length: Int, withSymbols: Bool = true) -> String {
+        var characters = letters + numbers
             characters += symbols
-        }        
-        return String((0..<length).compactMap{ _ in characters.randomElement() }) // Randomizes and finalizes the password
+        
+        return String((0..<length).compactMap{ _ in characters.randomElement() })
     }
 }
