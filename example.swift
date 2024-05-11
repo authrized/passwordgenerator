7881d07a33b3b74eaf0c2793dc3c0afe70947343
Elijah Friedman <elijah.friedman@authrized.com>
2024-05-11 07:28:27 -0400

    Create example.swift

---
+++ example.swift
@@ -0,0 +1,29 @@
+import SwiftUI
+
+struct ExampleView: View { 
+  
+    @State private var passwordLength: Double = 12
+    @State private var letters: String = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    @State private var numbers: String = "0123456789"
+    @State private var symbols: String = "!@#$%^&*(),.?\":{}|<>[]-_"
+    @State private var generatedPassword = "" 
+  
+    var body: some View {
+        VStack {
+            TextField("Letters", text: $letters)
+            TextField("Numbers", text: $numbers)
+            TextField("Symbols", text: $symbols)
+            Button("Generate Password") {
+                generatedPassword = generatePassword(length: Int(passwordLength))
+            }
+            Text(generatedPassword)
+        }
+    }
+  
+    func generatePassword(length: Int, withSymbols: Bool = true) -> String {
+        var characters = letters + numbers
+        characters += symbols
+        
+        return String((0..<length).compactMap{ _ in characters.randomElement() })
+    }
+}
